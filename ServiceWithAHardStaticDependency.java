9c21388f2b1a4bb6190bbd91df3fccc699780805
Antonio de la Torre Fernández <antondelatorre@gmail.com>
2019-02-07 10:33:51 +0100

    Change println by a return expression to express better the exercise intention

--- ServiceWithAHardStaticDependency.java
+++ ServiceWithAHardStaticDependency.java
@@ -11,7 +11,7 @@ class SomeService {
 
     private static String SOME_IMPORTANT_PARAMETER = Configuration.getPropertyFromDatabase("SOME_IMPORTANT_PARAMETER");
 
-    public void run() {
-        System.out.print(SOME_IMPORTANT_PARAMETER);
+    public String run() {
+        return SOME_IMPORTANT_PARAMETER;
     }
 }
